00c7016152c67771d6d713cbe5c51130459f4f37
Vitor Savian <vitor.savian@suse.com>
2024-02-22 19:37:32 -0300

    add kine-tls

--- pkg/etcd/etcd.go
+++ pkg/etcd/etcd.go
@@ -380,7 +380,6 @@ func (e *ETCD) Reset(ctx context.Context, rebootstrap func() error) error {
 					continue
 				}
 			}
-
 		}
 	}()
 
@@ -443,7 +442,7 @@ func (e *ETCD) Start(ctx context.Context, clientAccessInfo *clientaccess.Info) e
 	go e.manageLearners(ctx)
 
 	if isInitialized {
-		//check etcd dir permission
+		// check etcd dir permission
 		etcdDir := dbDir(e.config)
 		info, err := os.Stat(etcdDir)
 		if err != nil {
