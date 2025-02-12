9349014532d0a64c90dcd1178db928ebde79b915
muicoder <muicoder@gmail.com>
2025-02-12 11:52:42 +0800

    release-1.29
    Signed-off-by: muicoder <muicoder@gmail.com>

--- pkg/agent/flannel/setup.go
+++ pkg/agent/flannel/setup.go
@@ -48,8 +48,7 @@ const (
 
 	wireguardNativeBackend = `{
 	"Type": "wireguard",
-	"PersistentKeepaliveInterval": %PersistentKeepaliveInterval%,
-	"Mode": "%Mode%"
+	"PersistentKeepaliveInterval": 25
 }`
 
 	emptyIPv6Network = "::/0"
@@ -210,17 +209,6 @@ func createFlannelConf(nodeConfig *config.Node) error {
 	}
 
 	var backendConf string
-	backendOptions := make(map[string]string)
-
-	// precheck and error out unsupported flannel backends.
-	switch nodeConfig.FlannelBackend {
-	case config.FlannelBackendHostGW:
-	case config.FlannelBackendTailscale:
-	case config.FlannelBackendWireguardNative:
-		if goruntime.GOOS == "windows" {
-			return fmt.Errorf("unsupported flannel backend '%s' for Windows", nodeConfig.FlannelBackend)
-		}
-	}
 
 	switch nodeConfig.FlannelBackend {
 	case config.FlannelBackendVXLAN:
@@ -241,16 +229,7 @@ func createFlannelConf(nodeConfig *config.Node) error {
 		}
 		backendConf = strings.ReplaceAll(tailscaledBackend, "%Routes%", routes)
 	case config.FlannelBackendWireguardNative:
-		mode, ok := backendOptions["Mode"]
-		if !ok {
-			mode = "separate"
-		}
-		keepalive, ok := backendOptions["PersistentKeepaliveInterval"]
-		if !ok {
-			keepalive = "25"
-		}
-		backendConf = strings.ReplaceAll(wireguardNativeBackend, "%Mode%", mode)
-		backendConf = strings.ReplaceAll(backendConf, "%PersistentKeepaliveInterval%", keepalive)
+		backendConf = wireguardNativeBackend
 	default:
 		return fmt.Errorf("Cannot configure unknown flannel backend '%s'", nodeConfig.FlannelBackend)
 	}
